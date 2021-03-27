7836c407eb5ed7e7f00c13553de9ffe1dc4f782d
Carlos Maradiaga <carlos.maradiaga@hotmail.com>
2021-03-26 21:24:21 -0600

    Commit Inicial

---
+++ js/generic.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAJhB2DjN8T3sjTp_O8ehlmzvG1O5AOKAI",
+    authDomain: "proyectofirebase-79d05.firebaseapp.com",
+    projectId: "proyectofirebase-79d05",
+    storageBucket: "proyectofirebase-79d05.appspot.com",
+    messagingSenderId: "853593654323",
+    appId: "1:853593654323:web:daee604675f4b04f298f46",
+    measurementId: "G-H8QHHTTTK1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+function createUser() {
+
+    let email= document.getElementById("txtCorreo").value;
+    let password = document.getElementById("txtPassword").value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(res => {
+                alert("Se registró correctamente.");
+                document.getElementById("btnCancelar").click(); // para cerrar la ventana.
+            }).catch(eror => {
+                alert("Ocurrió un Error...");
+            });
+}
+
+function iniciarSesion() {
+
+    let email = document.getElementById("txtCorreoIngresar").value;
+    let password = document.getElementById("txtPasswordIngresar").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(res => {
+                // console.log(res); // para ver que propiedades tiene este objeto que devuelve firebase
+                document.location.href="/misPrestamos.html"; // Envia a esta pagina
+
+                // Imagen
+                if(res.user.photoUrl != null) {
+                    document.getElementById("imgFotoUsuario").src=res.user.photoUrl;
+                } else {
+                    document.getElementById("imgFotoUsuario").src="img/nousuario.jpg";
+                }
+            }).catch(err => {
+                document.getElementById("alertErrorLogueo").style.display="block";
+                document.getElementById("alertErrorLogueo").innerHTML = err;
+            });
+
+}
